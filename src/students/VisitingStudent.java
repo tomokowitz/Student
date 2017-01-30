32333345bcb1cf318c7d539212b534e0efee5c1d
tomokowitz <tomokowitz@gmail.com>
2017-01-30 13:41:15 -0500

    30 Jan 2017 CSC updates

--- src/students/VisitingStudent.java
+++ src/students/VisitingStudent.java
@@ -10,21 +10,23 @@ public class VisitingStudent extends Student
     public VisitingStudent()
     {
         super();
+        this.nativeLanguage = JOptionPane.showInputDialog("Enter language");
+        this.country = JOptionPane.showInputDialog("Enter country of origin");
     }
     public VisitingStudent(String firstName, String lastName, String SID, String language, String country )
     {
         super(firstName, lastName, SID);
         this.nativeLanguage = language;
-        this.lastName = lastName;
+        this.country = country;
        
     }
     
     
     public String toString() 
     {
-        return "Name: " + firstName + " " + lastName +
-               " SID: " + SID + " First language: " + nativeLanguage +
-               " Country: ";
+        return "Name: " + this.getFirstName() + " " + this.getLastName() +
+               " SID: " + this.getSID() + " First language: " + this.getLanguage()  +
+               " Country: " + this.getCountry();
     }
     
     public String getLanguage() 
